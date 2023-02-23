aa0d3f980e22953b58b1bf2869e66a3bf39a2609
MinnaDeVerdier <minna.deverdier@gmail.com>
2023-02-23 15:07:52 +0100

    skapat print() i class Land

--- Program.cs
+++ Program.cs
@@ -1,21 +1,26 @@
-﻿using System.Xml;
-
-namespace GrundläggandeObjekt_lander
+﻿namespace GrundläggandeObjekt_lander
 {
     internal class Program
     {
+
         public class Land
         {
-            public string namn = "", styrestyp = "", huvudstad = ""; 
-            public int invånare = 0; 
-        }
-        static void Main(string[] args)
-        {
-            Land sverige = new Land() { namn = "Sverige", styrestyp = "Monarki", huvudstad = "Stockholm", invånare = 505050}  ;
-            Land tyskland = new Land() { namn = "Tyskland", styrestyp = "republik", huvudstad = "Berlin", invånare = 898989 };
-            Land sanMarino = new Land() { namn = "San Marino", styrestyp = "republik", huvudstad = "San Marino", invånare = 333333 };
+            public string namn = "", styrestyp = "", huvudstad = "";
+            public int invånare = 0;
+            public void Print()
+            {
+                Console.WriteLine($"{namn}    {styrestyp}    {huvudstad}    {invånare}");
+            }
+            static void Main(string[] args)
+            {
+                Land sverige = new Land() { namn = "Sverige", styrestyp = "monarki", huvudstad = "Stockholm", invånare = 505050 };
+                Land tyskland = new Land() { namn = "Tyskland", styrestyp = "republik", huvudstad = "Berlin", invånare = 898989 };
+                Land sanMarino = new Land() { namn = "San Marino", styrestyp = "republik", huvudstad = "San Marino", invånare = 333333 };
 
-            Console.WriteLine("Hello, World!");
+                sverige.Print();
+                tyskland.Print();
+                sanMarino.Print();
+            }
         }
     }
 }
