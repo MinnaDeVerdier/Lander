b4236ec773690c87bd0ac9104adf8273daf0fd5d
MinnaDeVerdier <minna.deverdier@gmail.com>
2023-02-23 14:48:30 +0100

    lade till tre länder

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
-﻿namespace GrundläggandeObjekt_lander
+﻿using System.Xml;
+
+namespace GrundläggandeObjekt_lander
 {
     internal class Program
     {
         public class Land
         {
-            public Land() { string namn = "", styrestyp = "", huvudstad = ""; int invånare = 0; }
+            public string namn = "", styrestyp = "", huvudstad = ""; 
+            public int invånare = 0; 
         }
         static void Main(string[] args)
         {
+            Land sverige = new Land() { namn = "Sverige", styrestyp = "Monarki", huvudstad = "Stockholm", invånare = 505050}  ;
+            Land tyskland = new Land() { namn = "Tyskland", styrestyp = "republik", huvudstad = "Berlin", invånare = 898989 };
+            Land sanMarino = new Land() { namn = "San Marino", styrestyp = "republik", huvudstad = "San Marino", invånare = 333333 };
+
             Console.WriteLine("Hello, World!");
         }
     }
