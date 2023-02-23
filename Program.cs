bcfb12e1f5323bf686da5ee78d714761c1b865d2
MinnaDeVerdier <minna.deverdier@gmail.com>
2023-02-23 14:08:09 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GrundläggandeObjekt_lander
+{
+    internal class Program
+    {
+        public class Land
+        {
+            public Land() { string namn = "", styrestyp = "", huvudstad = ""; int invånare = 0; }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
